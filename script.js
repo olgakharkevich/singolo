038e5322ce95f1df9a52d3124f28e5f7f7b342b0
olgakharkevich <Olga.Kharkevich@gmail.com>
2020-03-18 23:23:56 +0300

    feat: add navigation

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
+// навигация в header по разделам (подсветка)
 const MENU = document.getElementById('menu');
 
-MENU.querySelectorAll('a');
+MENU.addEventListener('click', (event) => {
+    MENU.querySelectorAll('a').forEach(el => el.classList.remove('active'));
+    event.target.classList.add('active');
+});
 
-// MENU.addEventListener('click', (event) => {
-//     MENU.querySelectorAll('a').forEach()
-// })
+// переход по "якорям"
+document.addEventListener('scroll', onScroll);
+
+function onScroll(event) {
+    const curPos = window.scrollY;
+    const sections = document.querySelectorAll('section');
+    const links = document.querySelectorAll('#menu a');
+
+    sections.forEach((el) => {
+        if (el.offsetTop <= curPos && (el.offsetTop + el.offsetHeight) > curPos) {
+            links.forEach((a) => {
+                a.classList.remove('active');
+                if (el.getAttribute('id') === a.getAttribute('href').substring(1)) {
+                    a.classList.add('active');
+                }
+            })
+        }
+    })
+    
+}
+
+// слайдер
+
